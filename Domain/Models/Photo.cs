d0f01a5a161f60a23e2a8f2a54f10986276d7c08
temaykr99 <temaykr99@gmail.com>
2023-06-01 14:48:57 +0300

    добавил модели для БД

---
+++ Domain/Models/Photo.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Models
+{
+    public class Photo
+    {
+        public int Id { get; set; }
+
+        public string NameFile { get; set; }
+
+        [Column(TypeName = "nvarchar(MAX)")]
+        public string Base64 { get; set; }
+    }
+}
---
+++ Domain/Models/Сloth.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Models
+{
+    public class Сloth
+    {
+        public int Id { get; set; }
+        public string Price { get; set; }
+        public int CategoryClouthId { get; set; }
+        public CategoryClouth CategoryClouth { get; set; }
+        public int? PhotoId { get; set; }
+        public Photo Photo { get; set; }
+        public List<Size> Sizes { get; set; }
+
+    }
+}
